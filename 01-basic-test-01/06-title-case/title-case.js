dd50f2616f7f0f1fb7c85bedcdc5ee2d5ec0926f
shakirkhandakar <khandakarshakir.cse@gmail.com>
2025-12-10 21:39:09 +0600

    Add new functions

--- 01-basic-test-01/06-title-case/title-case.js
+++ 01-basic-test-01/06-title-case/title-case.js
@@ -12,6 +12,13 @@ function titleCase(str) {
     .join(" ");
 }
 
+function titleCase2(str) {
+  return str
+    .split(" ")
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(" ");
+}
+
 titleCase("hello world");
 
 module.exports = titleCase;
